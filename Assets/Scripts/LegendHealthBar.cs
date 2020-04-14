8ec85f8a20d451faf80c6ed8c54f78f0cc50b69f
Ubitroll <stephencartner@hotmail.co.uk>
2020-04-14 09:34:40 +0100

    UI stuff
    Minion bar script, Legend bar script

---
+++ Assets/Scripts/LegendHealthBar.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LegendHealthBar : MonoBehaviour
+{
+    // Variables
+    public Slider healthBar;
+    public Slider resourceBar;
+    public Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitialiseUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateUI();
+    }
+
+    // Initialise UI method
+    void InitialiseUI()
+    {
+        // If healthbar isn't null
+        if (healthBar != null)
+        {
+            healthBar.maxValue = this.GetComponentInParent<PlayerScript>().playerMaxHealth; 
+            healthBar.value = this.GetComponentInParent<PlayerScript>().playerMaxHealth;
+        }
+
+        // If resourcebar isn't null
+        if (resourceBar != null)
+        {
+            resourceBar.maxValue = this.GetComponentInParent<PlayerScript>().resourceMax;
+            resourceBar.value = this.GetComponentInParent<PlayerScript>().resourceMax;
+        }
+    }
+
+    // UI update method
+    void UpdateUI()
+    {
+        // Update to look at camera
+        transform.LookAt(transform.position + mainCamera.transform.rotation * Vector3.back, mainCamera.transform.rotation * Vector3.up);
+
+        // Update Health - Max is updated in case of item
+        healthBar.maxValue = this.GetComponentInParent<PlayerScript>().playerMaxHealth;
+        healthBar.value = this.GetComponentInParent<PlayerScript>().playerHealth;
+
+        // Update resources - Max is updated in case of item
+        resourceBar.maxValue = this.GetComponentInParent<PlayerScript>().resourceMax;
+        resourceBar.value = this.GetComponentInParent<PlayerScript>().resourceCurrent;
+    }
+}
---
+++ Assets/Scripts/MinionHealthBar.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MinionHealthBar : MonoBehaviour
+{
+    // Variables
+    public Slider healthBar;
+    public Camera mainCamera;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitialiseUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateUI();
+    }
+
+    // Initialise UI method
+    void InitialiseUI()
+    {
+        // If healthbar isn't null
+        if (healthBar != null)
+        {
+            healthBar.maxValue = this.GetComponentInParent<Minion>().Health;
+            healthBar.value = this.GetComponentInParent<Minion>().Health;
+        }
+    }
+
+    // UI update method
+    void UpdateUI()
+    {
+        // Update to look at camera
+        transform.LookAt(transform.position + mainCamera.transform.rotation * Vector3.back, mainCamera.transform.rotation * Vector3.up);
+
+        // Update Health - Max is updated in case of item
+        healthBar.maxValue = this.GetComponentInParent<Minion>().Health;
+        healthBar.value = this.GetComponentInParent<Minion>().Health;
+    }
+}
